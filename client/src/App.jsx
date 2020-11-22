e3d8c0954881b70107bf3f4316f932c0c2fdd58a
ifunesavaya <ifunes@simtlix.com>
2020-11-22 16:37:33 -0300

    side menu styling and few fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,20 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import Header from './components/Header'
-import './App.css'
 import Desktop from './components/breakpoints/Desktop'
 import Mobile from './components/breakpoints/Mobile'
 import SideMenu from './components/SideMenu'
 import Routes from './components/Routes'
-import { useState } from 'react'
-import { ButtonToggle } from 'reactstrap'
-import { GiHamburgerMenu } from 'react-icons/gi'
-
+import 'semantic-ui-css/semantic.min.css'
+import './App.css'
 
 const App = () => {
-
-    const [sidebarOpen, toggleSidebar] = useState(false);
-
     return (
         <Router>
             <Mobile>
-                <div style={{ zIndex: 10, position: 'absolute' }}>
-                    <ButtonToggle value={sidebarOpen} onClick={() => toggleSidebar(prevState => !prevState)}><GiHamburgerMenu size="1.5em"></GiHamburgerMenu></ButtonToggle>
-                </div>
-                <SideMenu open={sidebarOpen}>
+                <SideMenu>
                     <Routes></Routes>
                 </SideMenu>
             </Mobile>
-
             <Desktop>
                 <Header />
                 <Routes></Routes>
--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -1,34 +1,65 @@
-import React from 'react';
-import Sidebar from "react-sidebar";
-import { Link } from 'react-router-dom';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import React, { useRef, useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { Link, useLocation } from 'react-router-dom';
+import { ButtonToggle } from 'reactstrap';
+import { Sidebar, Menu, Ref, Segment } from 'semantic-ui-react';
+import '../css/SideMenu.css';
 
-const SideMenu = ({ children, ...props }) => {
+const SideMenu = ({ children }) => {
+    const segmentRef = useRef();
+    const [visible, setVisible] = useState(false);
+    const location = useLocation();
 
-    const SideBarContent = (
-        <div style={{height: '100vh'}}>
-            <ListGroup>
-                <ListGroupItem className="justify-content-between"><Link to='/'>HOME</Link></ListGroupItem>
-                <ListGroupItem className="justify-content-between"><Link to='/projects'>PROJECTS</Link></ListGroupItem>
-                <ListGroupItem className="justify-content-between"><Link to='/our-team'>OUR TEAM</Link></ListGroupItem>
-                <ListGroupItem className="justify-content-between"><Link to='/credits'>CREDITS</Link></ListGroupItem>
-            </ListGroup>
-            <div>
-                <img
-                    width="100%"
-                    alt="header-logo"
-                    src={process.env.PUBLIC_URL + "/header-logo.png"}
-                />
 
+    const isLocationActive = name => {
+        return location.pathname === name;
+    }
+    
+    return (
+        <Sidebar.Pushable as={Segment.Group} raised>
+            <div className="side-menu-header">
+                <ButtonToggle className="mobile-menu-button" color='white' onClick={() => setVisible(prev => !prev)}><GiHamburgerMenu size="1.5em"></GiHamburgerMenu></ButtonToggle>
+            <img
+                alt="header-logo"
+                className="mobile-header-logo"
+                src={process.env.PUBLIC_URL + "/header-logo.png"}
+            />
             </div>
-        </div>
-    )
+            <Sidebar
+                as={Menu}
+                animation='overlay'
+                icon='labeled'
+                onHide={() => setVisible(false)}
+                vertical
+                target={segmentRef}
+                visible={visible}
+                width='wide'
+                className="sidebar"
+            >
+                <Menu.Item active={isLocationActive('/')} onClick={() => setVisible(false)}>
+                    <Link to='/'>HOME</Link>
+                </Menu.Item>
+                <Menu.Item active={isLocationActive('/projects')}  onClick={() => setVisible(false)}>
+                    <Link to='/projects'>PROJECTS</Link>
+                </Menu.Item>
+                <Menu.Item active={isLocationActive('/our-team')} onClick={() => setVisible(false)}>
+                    <Link to='/our-team'>OUR TEAM</Link>
+                </Menu.Item>
+                <Menu.Item active={isLocationActive('/credits')} onClick={() => setVisible(false)}>
+                    <Link to='/credits'>CREDITS</Link>
+                </Menu.Item>
 
-    return (<Sidebar {...props} sidebar={
-        SideBarContent
-    } styles={{ sidebar: { background: "white", zIndex: 999 } }}>
-        {children}
-    </Sidebar>)
+            </Sidebar>
+
+            <Ref innerRef={segmentRef}>
+                <Sidebar.Pusher dimmed={visible}>
+                    <Segment>
+                        {children}
+                    </Segment>
+                </Sidebar.Pusher>
+            </Ref>
+
+        </Sidebar.Pushable>)
 
 }
 
