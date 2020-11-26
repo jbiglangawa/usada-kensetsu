756bc2ee5d774ebff32637dcb3fb52f93f836e71
ifunesavaya <ifunes@simtlix.com>
2020-11-26 00:00:08 -0300

    live subscribers count animations

--- client/src/components/LiveSubscribersCount.jsx
+++ client/src/components/LiveSubscribersCount.jsx
@@ -3,7 +3,6 @@ import { getSubscribersCount } from '../services/youtube.service';
 import socketIOClient from "socket.io-client";
 import '../css/LiveSubscribersCount.scss';
 import BunnyIcon from '../assets/svg/bunny-icon.svg'
-import RabbitShape from '../assets/svg/rabbit-shape.svg'
 import Small from '../assets/svg/small.svg';
 import Large from '../assets/svg/large.svg';
 import Carrot from '../assets/svg/carrot.svg';
@@ -12,22 +11,20 @@ import useScrollPosition from '@react-hook/window-scroll'
 
 const LiveSubscribersCount = () => {
 
-
     const [shouldAnimate, setShouldAnimate] = useState(false);
     const [count, setCount] = useState(0);
     const [prevCount, setPrevCount] = useState(0);
 
-    const useSlideAnimationOnScroll = (to, from, tension) => {
-        return {}
+    const useSlideAnimationOnScroll = (friction) => {
         return useSpring(shouldAnimate ? {
-            transform: `translateX(${to}%)`,
+            transform: `translateX(0%)`,
             visibility: 'visible',
             from: {
-                transform: `translateX(${from || 200}%)`
+                transform: `translateX(200%)`
             },
             config: {
                 tension: 200,
-                friction: tension || 50
+                friction: friction || 50
             }
         } : { visibility: 'hidden' });
     }
@@ -73,52 +70,49 @@ const LiveSubscribersCount = () => {
     return (
         <div ref={elementRef} className="subscribers-count-container">
             <div className="row-1">
-                <animated.div style={useSlideAnimationOnScroll(0)} className="title">
+                <animated.div style={useSlideAnimationOnScroll(150)}  className="title">
                     Current Nousagi <span className="employee-count-text">Employee Count</span>
                 </animated.div>
-                <div class="carrot">
-                    <animated.img src={Carrot} style={useSlideAnimationOnScroll(100, 700, 150)} className="carrot-opaque carrot-small"></animated.img>
-                </div>
+                <animated.div style={useSlideAnimationOnScroll(0)}  class="carrot">
+                    <img src={Carrot} className="carrot-opaque carrot-small"></img>
+                </animated.div>
             </div>
-            <div className="row-2">
+            <animated.div style={useSlideAnimationOnScroll(0)}  className="row-2">
                 <div className="carrot-2">
-                    <animated.img src={Carrot} style={useSlideAnimationOnScroll(100, 700, 200)} className="carrot carrot-opaque carrot-small carrot-2"></animated.img>
+                    <img src={Carrot} className="carrot carrot-opaque carrot-small carrot-2"></img>
                 </div>
                 <div className="carrot-3">
-                    <animated.img src={Carrot} style={useSlideAnimationOnScroll(-350, 700, 50)} className="carrot carrot-opaque carrot-small carrot-3"></animated.img>
+                    <img src={Carrot}  className="carrot carrot-opaque carrot-small carrot-3"></img>
                 </div>
                 <div className="rabbit-shape-small">
-                    <animated.img style={useSlideAnimationOnScroll(0, 0, 100)} src={Small}></animated.img>
+                    <img  src={Small}></img>
                 </div>
-            </div>
-            <div className="row-3">
-                {/* <div className="rabbit-shape" >
-                    <animated.img style={useSlideAnimationOnScroll(0, 0, 200)} src={RabbitShape}></animated.img>
-                </div> */}
-                <animated.div className="count-container">
-                    <animated.span style={{ ...useSlideAnimationOnScroll(0, 1500, 200), ...counterAnimator }} className="count">{counterAnimator.number.interpolate(count => Math.round(count).toLocaleString())}</animated.span>
-                    <animated.img style={useSlideAnimationOnScroll(0, 4000, 200)} className="bunny-icon" src={BunnyIcon}></animated.img>
-                </animated.div>
-            </div>
+            </animated.div>
+            <animated.div  style={useSlideAnimationOnScroll(200)}  className="row-3">
+                <div className="count-container">
+                    <animated.span style={counterAnimator} className="count">{counterAnimator.number.interpolate(count => Math.round(count).toLocaleString())}</animated.span>
+                    <img className="bunny-icon" src={BunnyIcon}></img>
+                </div>
+            </animated.div>
 
-            <div className="row-4">
-                <animated.div style={useSlideAnimationOnScroll(-70)} className="subscribe-button">
+            <animated.div style={useSlideAnimationOnScroll(0)} className="row-4">
+                <div  className="subscribe-button">
                     <span className="subscribe-text">Subscribe</span>
                     {/* <img src={Carrot} className="carrot"></img> */}
-                </animated.div>
-                <animated.div className="rabbit-shape">
-                    <animated.img style={useSlideAnimationOnScroll(0, 0, 150)} src={Large}></animated.img>
-                </animated.div>
+                </div>
+                <div className="rabbit-shape">
+                    <img src={Large}></img>
+                </div>
 
-            </div>
+            </animated.div>
 
-            <animated.div className="row-5">
+            <animated.div  style={useSlideAnimationOnScroll(0)}  className="row-5">
                 <div className="carrot-1">
-                    <animated.img src={Carrot} style={useSlideAnimationOnScroll(100, 700, 100)} className="carrot carrot-opaque"></animated.img>
+                    <img src={Carrot}  className="carrot carrot-opaque"></img>
                 </div>
 
                 <div className="carrot-2">
-                    <animated.img src={Carrot} style={useSlideAnimationOnScroll(300, 700, 120)} className="carrot carrot-opaque"></animated.img>
+                    <img src={Carrot} className="carrot carrot-opaque"></img>
                 </div>
 
             </animated.div>
