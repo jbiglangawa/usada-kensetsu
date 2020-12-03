193be1402a543d482bdff34a5748bf1c9b08caae
Nathanael <nael.nathanael71@gmail.com>
2020-12-03 23:04:17 +0800

    fix: loading screen still covers page while invisible

--- client/src/components/LoadingScreen.jsx
+++ client/src/components/LoadingScreen.jsx
@@ -1,15 +1,32 @@
+import React, { useEffect, useState } from 'react'
 import './LoadingScreen.css';
 
 const LoadingScreen = (props) => {
 
-    return props.renderStatus == 0 ? (
-        <div className="loadingScreenBase">
-            <div className="loadingScreenWrapper">
-                <div className='rabbit'></div>
-                <div className='clouds'></div>
+    const [animationComplete, setAnimationComplete] = useState(0);
+
+    useEffect(() => {
+        if (props.renderStatus == 1) {
+            removeLoading();
+        }
+    })
+
+    async function removeLoading() {
+        await new Promise(resolve => setTimeout(resolve, 500));
+        setAnimationComplete(1)
+    }
+
+    if (props.renderStatus == 0) {
+        return (
+            <div className="loadingScreenBase">
+                <div className="loadingScreenWrapper">
+                    <div className='rabbit'></div>
+                    <div className='clouds'></div>
+                </div>
             </div>
-        </div>
-    ) : (
+        )
+    } else if (props.renderStatus == 1 && animationComplete == 0) {
+        return (
             <div className="loadingScreenBase rollup">
                 <div className="loadingScreenWrapper">
                     <div className='rabbit'></div>
@@ -17,6 +34,8 @@ const LoadingScreen = (props) => {
                 </div>
             </div>
         )
+    }
+    return ""
 }
 
 export default LoadingScreen;
