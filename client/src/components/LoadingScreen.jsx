2f000586ae85e2ea08fdef882fe881b82c0524e0
Nathanael <nael.nathanael71@gmail.com>
2020-12-03 23:52:06 +0800

    feat: adjust loading screen to match requirements

--- client/src/components/LoadingScreen.jsx
+++ client/src/components/LoadingScreen.jsx
@@ -1,41 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import './LoadingScreen.css';
+import '../css/LoadingScreen.css';
 
 const LoadingScreen = (props) => {
 
-    const [animationComplete, setAnimationComplete] = useState(0);
-
-    useEffect(() => {
-        if (props.renderStatus == 1) {
-            removeLoading();
-        }
-    })
-
-    async function removeLoading() {
-        await new Promise(resolve => setTimeout(resolve, 500));
-        setAnimationComplete(1)
-    }
-
-    if (props.renderStatus == 0) {
-        return (
-            <div className="loadingScreenBase">
-                <div className="loadingScreenWrapper">
-                    <div className='rabbit'></div>
-                    <div className='clouds'></div>
+    return (
+        <div className="loading-grid">
+            <div className="loading">
+                <div className="loadingScreenBase">
+                    <div className="loadingScreenWrapper">
+                        <div className='rabbit'></div>
+                    </div>
                 </div>
+                <div className="loading-text-animation-label">Loading...</div>
             </div>
-        )
-    } else if (props.renderStatus == 1 && animationComplete == 0) {
-        return (
-            <div className="loadingScreenBase rollup">
-                <div className="loadingScreenWrapper">
-                    <div className='rabbit'></div>
-                    <div className='clouds'></div>
-                </div>
-            </div>
-        )
-    }
-    return ""
+        </div>
+
+    )
 }
 
 export default LoadingScreen;
