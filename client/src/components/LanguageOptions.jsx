047a64cb31c27e90441324fa5c40417131fc15c3
maruby <49580815+maruby@users.noreply.github.com>
2020-12-09 21:29:29 +0800

    change the display text on language options

--- client/src/components/LanguageOptions.jsx
+++ client/src/components/LanguageOptions.jsx
@@ -4,9 +4,9 @@ import { useTranslation } from 'react-i18next'
 
 const langOptions = [
   { key: 'English', text: 'ENG', value: 'en' },
-  { key: 'Indonesian', text: 'INDO', value: 'id' },
+  { key: 'Indonesian', text: 'ID', value: 'id' },
   { key: 'Japanese', text: '日本語', value: 'ja' },
-  { key: 'Spanish', text: 'SPA', value: 'es' },
+  { key: 'Spanish', text: 'ESP', value: 'es' },
 ]
 
 const LanguageOptions = (mobile) => {
