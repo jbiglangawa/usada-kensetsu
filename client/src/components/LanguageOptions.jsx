d374d9aa516e08e49e5012e15a51ea5ff59b508b
maruby <49580815+maruby@users.noreply.github.com>
2020-12-09 23:06:34 +0800

    minor ui changes and display selected language on load

--- client/src/components/LanguageOptions.jsx
+++ client/src/components/LanguageOptions.jsx
@@ -4,15 +4,14 @@ import { useTranslation } from 'react-i18next'
 
 const langOptions = [
   { key: 'English', text: 'ENG', value: 'en' },
+  { key: 'Spanish', text: 'ESP', value: 'es' },
   { key: 'Indonesian', text: 'ID', value: 'id' },
   { key: 'Japanese', text: '日本語', value: 'ja' },
-  { key: 'Spanish', text: 'ESP', value: 'es' },
 ]
 
 const LanguageOptions = (mobile) => {
     const [t, i18n] = useTranslation()
     const pointer = mobile ? "top right" : false
-    const currentLanguage = langOptions.map(lang => i18n.language === lang.value ? lang.text : null)
 
     const changeLanguage = (e, {value}) => {
         i18n.changeLanguage(value)
@@ -27,7 +26,7 @@ const LanguageOptions = (mobile) => {
             floating
             labeled
             options={langOptions}
-            text={currentLanguage}
+            defaultValue={i18n.language}
         />
         </>
     )
