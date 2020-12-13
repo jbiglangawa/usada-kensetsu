2716ace2432bf9a8b48bb4cc5aec994f97e3ea5f
Nathanael <nael.nathanael71@gmail.com>
2020-12-14 01:50:10 +0800

    feat: create Usada Pekora Video data emitter

--- routes/youtube.js
+++ routes/youtube.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const { getCurrentCount } = require('../controllers/youtube');
+const { getCurrentCount, getPekoraMinecraftVideoList, getPekoraAllVideoList } = require('../controllers/youtube');
 const router = express.Router();
 
 router.get('/current-subscribers-count', function(req, res, next) {
-  res.send({count: getCurrentCount()});
+    res.send({ count: getCurrentCount() });
+});
+
+router.get('/pekora-minecraft-video', function(req, res, next) {
+    res.send({ VideoList: getPekoraMinecraftVideoList() });
+});
+
+router.get('/pekora-all-video', function(req, res, next) {
+    res.send({ VideoList: getPekoraAllVideoList() });
 });
 
 module.exports = router;
